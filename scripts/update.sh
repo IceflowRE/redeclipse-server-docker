fc96f41f1fdb0bbf5c6fcd8e9a574066fa68dca2
Iceflower <iceflower@gmx.de>
2017-08-25 16:36:48 +0200

    Fix errors

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,10 +3,10 @@
 # variable BRANCH is defined inside the travis.yml env.
 SHA="$(git ls-remote --heads https://github.com/red-eclipse/base.git master | awk '{ print $1 }')"
 
-if [ "$(cat ~/$BRANCH.sha)" != "$SHA" ]; then
+if [ "$(cat ./$BRANCH.sha)" != "$SHA" ]; then
     echo "Build $BRANCH"
-    #./build-docker.sh "devel"
-    echo "$SHA" > "~/$BRANCH.sha"
+    ./scripts/build-docker.sh "$BRANCH"
+    echo "$SHA" > "./$BRANCH.sha"
 else
     echo "Skip $BRANCH"
 fi
