f88681c8f2704c9978cd050636edf836387f4095
Unknown <iceflower@gmx.de>
2017-08-25 19:15:09 +0200

    Use correct shell var names and add fail checking

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-REPO="iceflower/red-eclipse_devel_server_test"
-BRANCH="$1"  # master or stable, given as argument
+repo="iceflower/red-eclipse_devel_server_test"
+branch="$1"  # master or stable, given as argument
 # later use one dockerfile, but stable has no cmake support atm
-#docker build --squash -t "$REPO:$BRANCH" -f "Dockerfile_$BRANCH" .
-docker build -t "$REPO:$BRANCH" -f "Dockerfile_$BRANCH" .
-docker push "$REPO:$BRANCH"
+#docker build --squash -t "$repo:$branch" -f "Dockerfile_$branch" .
+docker build --build-arg BRANCH="$branch" -t "$repo:$branch" -f "Dockerfile_$branch" .
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+docker push "$repo:$branch"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 exit 0
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 # variable BRANCH is defined inside the travis.yml env.
-SHA="$(git ls-remote --heads https://github.com/red-eclipse/base.git master | awk '{ print $1 }')"
+sha="$(git ls-remote --heads https://github.com/red-eclipse/base.git master | awk '{ print $1 }')"
 
-if [ "$(cat ./sha/$BRANCH.sha)" != "$SHA" ]; then
+if [ "$(cat ./sha/$BRANCH.sha)" != "$sha" ]; then
     echo "Build $BRANCH"
     ./scripts/build-docker.sh "$BRANCH"
 # dont save sha if something failed
     if [ $? -ne 0 ]; then
         exit 1
     fi
-    echo "Save sha $SHA as $BRANCH"
-    echo "$SHA" > "./sha/$BRANCH.sha"
+    echo "Save sha $sha as $BRANCH"
+    echo "$sha" > "./sha/$BRANCH.sha"
 else
-    echo "Skip $BRANCH"
+    echo "Skip $BRANCH - $sha"
 fi
 exit 0
