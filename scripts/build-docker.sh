70c32bd0e7e1052a47a80de2f108eca114830eae
Iceflower <iceflower@gmx.de>
2017-09-02 16:45:36 +0200

    Change docker hub repo

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -3,7 +3,7 @@
 #
 # variable branch and prefix will be passed
 
-repo="iceflower/red-eclipse_devel_server_test"
+repo="iceflower/redeclipse-server"
 # later use one dockerfile, but stable has no cmake support atm
 #docker build --squash --build-arg BRANCH="$branch" -t "$repo:$prefix$branch" -f "Dockerfile_$branch" .
 docker build --build-arg PREIMAGE="$preimage" --build-arg BRANCH="$branch" -t "$repo:$prefix$branch" -f "Dockerfile_$branch" .
