9a9d08c80f6b58d0c87a292eb83337f3f2e509a6
Iceflower <iceflower@gmx.de>
2018-09-29 00:20:12 +0200

    Fix builder

--- scripts/update-server.sh
+++ scripts/update-server.sh
@@ -2,6 +2,6 @@
 
 docker-compose stop -t 600 master stable
 #docker-compose down
-./scripts/my-builder.sh
+./scripts/my-builder.sh "$1"
 docker-compose -p re-server up -d master
 docker-compose -p re-server up -d stable
