b6b10bb2b8fada49b70ab0b6554446418b96de68
Iceflower <iceflower@gmx.de>
2020-02-03 23:19:35 +0100

    Script updates

--- scripts/update-server.sh
+++ scripts/update-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-docker-compose stop -t 600 master stable
+docker-compose stop -t 600 master stable stable-re2
 #docker-compose down
 rsd-updater /home/iceflower/.re-updater/ --config
 docker-compose -p re-server up -d master
 docker-compose -p re-server up -d stable
+docker-compose -p re-server up -d stable-re2
